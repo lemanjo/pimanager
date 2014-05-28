d71aa2d31c9e9b4f4147c7147a6f88cbb07a4d16
J.Manu <jussi.manu@gmail.com>
2014-05-29 00:02:46 +0300

    Git ignore update

--- pimanager/settings.py
+++ pimanager/settings.py
@@ -10,6 +10,13 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Function for correct server timezone
 from lib.timezone import gettz
 
+# Import sensitive information
+try:
+    from settings_secret import *
+
+except ImportError:
+    pass
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
